fa2238b62ebfdb88d8791a3b3f936e5dec10b900
Darshan Sundaresh <51130893+d5n@users.noreply.github.com>
2021-03-15 16:57:46 +0530

    [semver:minor] add functionality to run webscans (#7)
    * new html reports
    * new web-scan job and commands
    * ability to gate builds based on report

--- src/scripts/verify.sh
+++ src/scripts/verify.sh
@@ -30,20 +30,54 @@ if [[ "$status" != "0" ]]; then
    exit 1
 fi
 
+mkdir /tmp/report
 
 # Get application
-application_id=$( curl -ks -X GET "${DF_PORTAL_URL}/api/services/v1/applications" -H "Authorization: Bearer ${DF_API_KEY}" | jq -r --arg v "${DF_APP}" '.data.apps[] | select (.name==$v) | .application_id' )
+application_id=$( curl -ks -X GET "${DF_PORTAL_URL}/api/services/v1/applications" \
+                    -H "Authorization: Bearer ${DF_API_KEY}" | \
+                    jq -r --arg v "${DF_APP}" '.data.apps[] | select (.name==$v) | .application_id' )
+
 echo "application_id=${application_id}"
 
 # Get components
-component_version_tuple=$( curl -ks -X GET "${DF_PORTAL_URL}/api/services/v1/applications/${application_id}/components" -H "Authorization: Bearer ${DF_API_KEY}" | jq -r --arg c "${DF_COMPONENT}" '.data.components[] | select (.name==$c) | {component_id,latest_component_version,security}')
+component_version_tuple=$( curl -ks -X GET \
+                            "${DF_PORTAL_URL}/api/services/v1/applications/${application_id}/components" \
+                            -H "Authorization: Bearer ${DF_API_KEY}" | \
+                            jq -r --arg c "${DF_COMPONENT}" \
+                            '.data.components[] | select (.name==$c) | {component_id,latest_component_version,security}')
+
 component_id=$( jq -r '.component_id' <<< "$component_version_tuple" )
 echo "component_id=${component_id}"
 
 component_security_summary=$( jq -r '.security' <<< "$component_version_tuple")
 
 summary=$(jq -r '{ total: .total, p1: .p1, p2: .p2, p3: .p3, p4: .p4 }' <<< "$component_security_summary")
-cat <<< "$summary" > deepfactor_summary.json
+cat <<< "$summary" > /tmp/report/deepfactor_summary.json
 
-alert_details=$(curl -ks -H "Authorization: Bearer ${DF_API_KEY}" -X GET "${DF_PORTAL_URL}/api/services/v1/alerts?application_id=${application_id}&component_id=${component_id}&version_type=latest" | jq -r --arg portal "${DF_PORTAL_URL}" '[.data.alerts[] | {alertType: .alertType, severity: .severity, title: .title ,dfa: .dfa, link: ($portal + "/alerts/" + .dfa)}]' )
-cat <<< "$alert_details" > deepfactor_alert_list.json
+alert_details=$(curl -ks -X GET "${DF_PORTAL_URL}/api/services/v1/alerts?application_id=${application_id}&component_id=${component_id}&version_type=latest" \
+                    -H "Authorization: Bearer ${DF_API_KEY}" \
+                    | jq -r --arg portal "${DF_PORTAL_URL}" \
+                    '[.data.alerts[] | {alertType: .alertType, severity: .severity, title: .title ,dfa: .dfa, link: ($portal + "/alerts/" + .dfa)}]' )
+
+cat <<< "$alert_details" > /tmp/report/deepfactor_alert_list.json
+
+wget https://ci-assets.deepfactor.io/index.html -O /tmp/report/index.html
+
+echo "${DF_FAIL_SEVERITY}"
+if [ "${DF_FAIL_SEVERITY}" != 'disabled' ]; then
+  p1=$( jq '.p1' < /tmp/report/deepfactor_summary.json )
+  if [[ ${DF_FAIL_SEVERITY} == 'p1' ]] && [[ "$p1" -gt 0 ]]; then
+    echo 'DeepFactor detected alerts above threshold P1. Failing build'
+    exit 1
+  fi
+  p2=$( jq '.p2' < /tmp/report/deepfactor_summary.json )
+  if [[ "${DF_FAIL_SEVERITY}" == 'p2' ]] && [[ "$p2" -gt 0 || "$p1" -gt 0 ]]; then
+    echo 'DeepFactor detected alerts above threshold P2. Failing build'
+    exit 1
+  fi
+  p3=$( jq '.p3' < /tmp/report/deepfactor_summary.json )
+  if [[ "${DF_FAIL_SEVERITY}" == 'p3' ]] && [[ "$p3" -gt 0 ||  "$p2" -gt 0 ||  "$p1" -gt 0 ]]; then
+    echo 'DeepFactor detected alerts above threshold P3. Failing build'
+    exit 1
+  fi
+fi
---
+++ src/scripts/webscan.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+echo DF_API_KEY="${API_KEY}" >> "${BASH_ENV}"
+
+if [[ -z "$DF_API_KEY" ]]; then
+    echo "DF_API_KEY environment variable missing"
+    exit 1
+fi
+
+if [[ -z "$DF_PORTAL_URL" ]]; then 
+    echo "Param \"portal-url\" is missing"
+    exit 1
+fi
+
+if [[ -z "$DF_APP" ]]; then
+    echo "Param \"app-name\" is missing"
+    exit 1
+fi
+
+if [[ -z "$DF_COMPONENT" ]]; then
+    echo "Param \"component-name\" is missing"
+    exit 1
+fi
+
+# check the portal url is valid
+curl -kfs "${DF_PORTAL_URL}/api/health" > /dev/null
+status="$?"
+if [[ "$status" != "0" ]]; then
+   echo "The param DeepFactor portal_url is incorrect or is not reachable"
+   exit 1
+fi
+
+if [[ -z "$DF_SCAN_CONFIG_TYPE" ]]; then
+    echo "Param \"scan-type\" is missing"
+    exit 1
+fi
+
+if [[ "$DF_SCAN_CONFIG_TYPE" == "owasp-zap-api" ]] && [[ -z "$DF_SCAN_API_DOCS_URL" ]]; then
+    echo "Param \"scan-api-docs-url\" is required for the api scans"
+    exit 1
+fi
+
+if [[ -z "$DF_SCAN_CONFIG_STRENGTH" ]]; then 
+    echo "Param \"scan-strength\" is missing"
+    exit 1
+fi
+
+if [[ -z "$DF_SCAN_CONFIG_AUTH_TYPE" ]]; then
+    echo "Param \"scan-auth-type\" is missing"
+    exit 1
+fi
+
+if [[ -z "$DF_SCAN_CONFIG_SCAN_URL" ]]; then
+    echo "Param \"scan-url\" is missing"
+    exit 1
+fi 
+
+if [[ -z "$DF_COMPONENT_VERSION" ]]; then
+    DF_COMPONENT_VERSION="${CIRCLE_BUILD_NUM}"
+fi
+echo "compVersion:${DF_COMPONENT_VERSION} circleciBuild:${CIRCLE_BUILD_NUM}"
+
+SCAN_HOST=$( echo "$DF_SCAN_CONFIG_SCAN_URL" | sed -e 's/[^/]*\/\/\([^@]*@\)\?\([^:/]*\).*/\2/' )
+
+generate_start_scan_request()
+{
+    cat <<EOF
+{
+    "application_name":"$DF_APP",
+    "component_name":"$DF_COMPONENT",
+    "component_version":"$DF_COMPONENT_VERSION",
+    "scan_config":
+    {
+        "scan_type":"$DF_SCAN_CONFIG_TYPE",
+        "active_scan_policy_strength":"$DF_SCAN_CONFIG_STRENGTH",
+        "auth_type":"$DF_SCAN_CONFIG_AUTH_TYPE",
+        "external_scan_url":"$DF_SCAN_CONFIG_SCAN_URL",
+        "api_scan_url":"$DF_SCAN_API_DOCS_URL",
+        "host":"$SCAN_HOST"
+    }
+}
+EOF
+
+}
+
+status=$( curl -ks -X POST --data \
+            "$(generate_start_scan_request)" \
+            --write-out '%{http_code}' \
+            --output /dev/null \
+            "${DF_PORTAL_URL}/api/services/v1/webservices/scans" \
+            -H "Authorization: Bearer ${DF_API_KEY}" )
+if [ "$status" != 200 ]; then
+    exit 1
+fi
